c5c49be9e2c2fc57b96af4d0241b147427925e49
mpociuipa <manthis.poc@gmail.com>
2024-07-09 20:38:00 +0300

    Header Footer Auth

---
+++ fancy logger/time-logger/src/components/header/Header.jsx
@@ -0,0 +1,97 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth, logout } from "../../services/AuthServices";
+import "./Header.scss";
+
+const Header = () => {
+  const [user] = useAuthState(auth);
+  const [isNavCollapsed, setIsNavCollapsed] = useState(true);
+
+  const handleNavToggle = () => {
+    setIsNavCollapsed(!isNavCollapsed);
+  };
+
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">
+          Time-logger
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          onClick={handleNavToggle}
+          aria-controls="navbarNav"
+          aria-expanded={!isNavCollapsed}
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div
+          className="collapse navbar-collapse justify-content-end"
+          id="navbarNav"
+        >
+          {user && (
+            <>
+              <Link to="/darbo-tipas" className="dropdown-item">
+                Darbo tipas
+              </Link>
+
+              <Link to="/projektas" className="dropdown-item">
+                Projektų sąrašas
+              </Link>
+
+              <Link to="/atliktas-darbas" className="dropdown-item">
+                Alikti darbai
+              </Link>
+            </>
+          )}
+        </div>
+        <div
+          className={`collapse navbar-collapse justify-content-end ${
+            isNavCollapsed ? "collapse" : "show"
+          }`}
+          id="navbarNav"
+        >
+          <ul className="navbar-nav">
+            {!user ? (
+              <>
+                <li className="nav-item">
+                  <Link to="/reset" className="nav-link">
+                    Reset
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/login" className="nav-link">
+                    Prisijungti
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/register" className="nav-link">
+                    Registruotis
+                  </Link>
+                </li>
+              </>
+            ) : (
+              <>
+                <li className="nav-item">
+                  <Link to="/profile" className="nav-link">
+                    Profilis
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <button className="nav-link btn" onClick={logout}>
+                    Atsijungti
+                  </button>
+                </li>
+              </>
+            )}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
